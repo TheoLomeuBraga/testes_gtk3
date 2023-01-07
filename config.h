af3368f38db676a07ea2eea423d7fd5e1b22fa8c
TheoLomeuBraga <theolomeu@gmail.com>
2023-01-06 21:38:44 -0300

    a

--- config.h
+++ config.h
@@ -1,1 +1,7 @@
 int icon_res = 32;
+int res_x=1080,res_y=1080;
+int add_pos_y = 0;
+
+
+bool aways_active = false,skip_taskbar_hint = false,centralize = true;
+
--- theogtkpanel.cpp
+++ theogtkpanel.cpp
@@ -8,11 +8,21 @@ using namespace std;
 vector<shared_ptr<component>> components = {
   make_shared<launcher>(launcher("/home/theo/testes_gtk3/built/icons/browser.png","firefox","firefox &")),
   make_shared<launcher>(launcher("thunar","thunar","thunar &")),
-  //make_shared<launcher>(launcher("","firefox","firefox &")),
+  make_shared<launcher>(launcher("thunar","thunar","thunar &")),
+  make_shared<launcher>(launcher("thunar","thunar","thunar &")),
+  make_shared<launcher>(launcher("thunar","thunar","thunar &")),
+  make_shared<launcher>(launcher("thunar","thunar","thunar &")),
+  make_shared<launcher>(launcher("thunar","thunar","thunar &")),
 };
 
 
-
+void load_setings(string path){
+  GdkRectangle workarea = {0};
+  gdk_monitor_get_workarea(gdk_display_get_primary_monitor(gdk_display_get_default()),&workarea);
+  res_x = workarea.width;
+  res_y = workarea.height;
+  
+}
 
 
 int main (int argc,char **argv)
@@ -20,7 +30,7 @@ int main (int argc,char **argv)
   gtk_init(&argc, &argv);
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   
-
+  load_setings("./config.json");
   
   //theme
   GtkSettings *default_settings = gtk_settings_get_default();
@@ -29,11 +39,11 @@ int main (int argc,char **argv)
   gtk_window_set_title (GTK_WINDOW (window), "hello world");
   //ocultar , posicionar e sobre posicionar
   gtk_window_set_decorated(GTK_WINDOW(window),false);
-  gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window),true);
-  //gtk_window_set_keep_above(GTK_WINDOW(window),true);
-  gtk_window_set_default_size (GTK_WINDOW (window), 100, 400);
-  gtk_window_move(GTK_WINDOW (window),0,100);
-  //gtk_window_set_position(GTK_WINDOW (window),GTK_WIN_POS_CENTER_ALWAYS);
+  gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window),skip_taskbar_hint);
+  gtk_window_set_keep_above(GTK_WINDOW(window),aways_active);
+  gtk_window_set_default_size (GTK_WINDOW (window), icon_res, icon_res*components.size());
+  gtk_window_move(GTK_WINDOW (window),0,((res_y/2 - icon_res/2)/1.5) + add_pos_y);
+  if(centralize){gtk_window_set_position(GTK_WINDOW (window),GTK_WIN_POS_CENTER_ALWAYS);}
 
   GtkWidget *vbox = gtk_vbox_new(TRUE, 1);
   gtk_container_add(GTK_CONTAINER(window), vbox);
