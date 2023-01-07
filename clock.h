6521bd19033791350924f8fb7ff56e2643a0b608
TheoLomeuBraga <theolomeu@gmail.com>
2023-01-07 00:47:28 -0300

    a

---
+++ clock.h
@@ -0,0 +1,20 @@
+using namespace std;
+#include <gtk/gtk.h>
+#include <iostream>
+#include "component.h"
+#include "config.h"
+
+class clock : public component{
+public:
+    
+    clock(){}
+
+    clock(){}
+
+    
+    static void run_command (GtkWidget *widget,gpointer data)
+    {
+    }
+    void render(GtkWidget *container){
+    }
+};
