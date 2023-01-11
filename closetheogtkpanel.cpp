cd2650717f7152fd0cf38d225a2cc7d3ed32cec7
TheoLomeuBraga <theolomeu@gmail.com>
2023-01-11 16:40:23 -0300

    a

---
+++ closetheogtkpanel.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+#include <fstream>
+#include <iostream>
+#include <fstream>
+
+int main (int argc,char **argv){
+    system("pkill theogtkpanel");
+    return 0;
+}
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo rm /bin/theogtkpanel
 sudo cp ./built/theogtkpanel /bin
+
+sudo rm /bin/closetheogtkpanel
+sudo cp ./built/closetheogtkpanel /bin
